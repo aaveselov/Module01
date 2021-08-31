426029ae36719006ecec2d1b9577cd7bd9f6c972
aaveselov@mail.ru <aaveselov@mail.ru>
2021-08-31 15:03:06 +0300

    hotfix added ! at the end

--- src/IPVeselovAA/Main.java
+++ src/IPVeselovAA/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String STR = System.getenv().getOrDefault("STR", "");
-        System.out.println("Hello " + STR);
+        System.out.println("Hello " + STR + "!");
     }
 }
