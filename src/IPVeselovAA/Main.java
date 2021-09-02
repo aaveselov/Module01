42b2520f2784261db85525cae41994c953194d54
aaveselov@mail.ru <aaveselov@mail.ru>
2021-09-02 16:04:08 +0300

    simulate bugfix (not release) tag

--- src/IPVeselovAA/Main.java
+++ src/IPVeselovAA/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String STR = System.getenv().getOrDefault("STR", "");
         System.out.println("Hello " + STR + "!");
+//fixed a bug here, not ready to release yet
     }
 }
